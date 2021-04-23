f73eb78e08d917b8a4790169b7fa852be7853e84
Sam Cohen <samc1213@gmail.com>
2021-04-22 21:51:34 -0700

    add table

--- js/campus-vacc-main.js
+++ js/campus-vacc-main.js
@@ -17,6 +17,18 @@ String.prototype.splitCSV = function() {
     return matches;
 }
 
+let table = document.getElementById('table');
+let header = table.createTHead();
+let row = header.insertRow(0);
+let cell = row.insertCell(0);
+cell.innerText = 'Name';
+cell = row.insertCell(1);
+cell.innerText = 'City';
+cell = row.insertCell(2);
+cell.innerText = 'State';
+cell = row.insertCell(3);
+cell.innerText = 'Vaccine Req. Announcement';
+
 fetch('https://raw.githubusercontent.com/samc1213/college-vacc/main/scripts/output/colleges.csv')
 .then(r => r.text()).then(text => {
     text.split(/\r?\n/).slice(1).forEach(line => {
@@ -31,6 +43,8 @@ fetch('https://raw.githubusercontent.com/samc1213/college-vacc/main/scripts/outp
         }
         let name = lineSplit[1];
         let url = lineSplit[2];
+        let city = lineSplit[3];
+        let state = lineSplit[4];
         let lat = parseFloat(lineSplit[5]);
         let lon = parseFloat(lineSplit[6]);
 
@@ -43,6 +57,16 @@ fetch('https://raw.githubusercontent.com/samc1213/college-vacc/main/scripts/outp
         let newMarker = L.marker([lat, lon], {icon: icon});
         newMarker.bindPopup(`<div class="school-popup"><h4>${name}</h4><a href="${announcement_url}" target="_blank">Announcement</a><br> <img src="https://logo.clearbit.com/${url}" /></div>`)
         markers.addLayer(newMarker);
+
+        var newTableRow = table.insertRow(-1);
+        let cell = newTableRow.insertCell(0);
+        cell.innerText = name;
+        cell = newTableRow.insertCell(1);
+        cell.innerText = city;
+        cell = newTableRow.insertCell(2);
+        cell.innerText = state;
+        cell = newTableRow.insertCell(3);
+        cell.innerHTML = `<a href="${announcement_url}">Link</a>`;
     })
 });
 
